8822fc3c12dcd62a11f5cd95173e7bd3572d16e6
TiavinaTiTi <titus50231@gmail.com>
2024-06-13 10:15:55 +0300

    Ajouter les routLink dans navbar et root

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
   dropdownMenu = [
-    {title: 'Our Menu', label: 'Our Menu', icon: '', root: ''},
-    {title: 'About Us', label: 'About Us', icon: '', root: ''},
-    {title: 'Contact Us', label: 'Contact Us', icon: '', root: ''},
+    {title: 'Our Menu', label: 'Our Menu', icon: '', root: '/menu'},
+    {title: 'About Us', label: 'About Us', icon: '', root: '/about'},
+    {title: 'Contact Us', label: 'Contact Us', icon: '', root: 'contact'},
   ]
 
   itemMenu = [

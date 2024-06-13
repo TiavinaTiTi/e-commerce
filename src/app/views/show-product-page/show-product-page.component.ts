8350b20290cd50a8bd8c4d9f5142c1c98416bcca
TiavinaTiTi <titus50231@gmail.com>
2024-06-13 13:37:39 +0300

    Creation fonction ajout et soustraction quantiter produit showPageProduct

--- src/app/views/show-product-page/show-product-page.component.ts
+++ src/app/views/show-product-page/show-product-page.component.ts
@@ -5,24 +5,36 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {ProductService} from "../../shared/services/productService/product.service";
 import {toNumbers} from "@angular/compiler-cli/src/version_helpers";
 import {Observable} from "rxjs";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-show-product-page',
   standalone: true,
   imports: [
     UpperCasePipe,
-    AsyncPipe
+    AsyncPipe,
+    FormsModule
   ],
   templateUrl: './show-product-page.component.html',
   styleUrl: './show-product-page.component.scss'
 })
 export class ShowProductPageComponent {
-
   activeRoute:ActivatedRoute = inject(ActivatedRoute)
   productService: ProductService = inject(ProductService)
 
   starAll: number[] = Array.from(Array(6).keys())
   private _id = (this.activeRoute.snapshot.paramMap.get('id')) ?? '0'
   @Input() product$: Observable<ProductModel | undefined> = this.productService.getProductId(parseInt(this._id))
+  quantity: number = 1
+
+  addQuantity(){
+    this.quantity += 1
+  }
+
+  subtractQuantity(){
+    if(this.quantity > 1){
+      this.quantity -= 1
+    }
+  }
 
 }
